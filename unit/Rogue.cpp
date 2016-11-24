69d3480c0c90c58c4a8685926076531ae2653bd8
bor16 <kalininboris@gmail.com>
2016-11-24 16:03:47 +0200

    [Rogue] Rename physDmg to damage

--- unit/Rogue.cpp
+++ unit/Rogue.cpp
@@ -1,17 +1,11 @@
 #include "Rogue.h"
 
-Rogue::Rogue(const std::string& name, const std::string& unitClass, int maxHp, int physDmg) : Soldier(name, unitClass, maxHp, physDmg) {
-    std::cout << "***" << this->name << " the " << this->unitClass << "___" << std::endl;
-}
+Rogue::Rogue(const std::string& name, const std::string& unitClass, int maxHp, int damage) : Soldier(name, unitClass, maxHp, damage) {}
 
-Rogue::~Rogue() {
-    std::cout << "~~~" << this->name << " the " << this->unitClass << "___" << std::endl;
-}
+Rogue::~Rogue() {}
 
 void Rogue::attack(Unit& enemy) {
-    if ( this->dead() ) {
-        // throw DeadAttackException("ERROR: zombie attack");
-    }
+    this->state->ensureIsAlive();
     
-    enemy.takeDamage(this->getPhysDmg(), 0);
+    enemy.takeDamage(this->getDamage());
 }
