7e3f7b04f8ab8f0b3386f2b3365a3ee0f6f744f2
bor16 <kalininboris@gmail.com>
2016-11-29 17:34:05 +0200

    [Vampire] Add Vampire class

---
+++ unit/Vampire.cpp
@@ -0,0 +1,23 @@
+#include "Vampire.h"
+
+Vampire::Vampire(const std::string& name, const std::string& unitClass, int maxHp, int damage) : Soldier(name, unitClass, maxHp, damage) {}
+
+Vampire::~Vampire() {}
+
+void Vampire::attack(Unit& enemy) {
+    int points = enemy.getHp();
+    
+    this->state->ensureIsAlive();
+    
+    enemy.takeDamage(this->getDamage());
+    points -= enemy.getHp();
+    this->restoreHp(points/2);
+}
+
+void Vampire::counterAttack(Unit& enemy) {
+    int points = enemy.getHp();
+    
+    enemy.takeDamage(this->getDamage()/2);
+    points -= enemy.getHp();
+    this->restoreHp(points/2);
+}
