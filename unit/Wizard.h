8f0008efa6e12f37b483da3e39bc892157bb48d7
bor16 <kalininboris@gmail.com>
2016-11-29 14:16:08 +0200

    [Wizard] Add Wizard class

---
+++ unit/Wizard.h
@@ -0,0 +1,26 @@
+#ifndef WIZARD_H
+#define WIZARD_H
+
+#include <iostream>
+#include "Soldier.h"
+#include "../state/CasterState.h"
+#include "../spell/Spell.h"
+
+class Wizard : public Soldier {
+    protected:
+        CasterState* state;
+    
+    public:
+        Wizard(const std::string& name, const std::string& unitClass="Wizard", int maxHp=50, int damage=3, int maxMana=100);
+        virtual ~Wizard();
+        
+        const int getMana() const;
+        const int getMaxMana() const;
+        
+        void reduceMana(int cost);
+        void restoreMana(int points);
+        
+        void cast(Unit& target, Spell& spell);
+};
+
+#endif //WIZARD_H
