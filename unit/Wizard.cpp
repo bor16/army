6f5daaaedd581c91c22327f30918e195e9a19404
bor16 <kalininboris@gmail.com>
2016-12-01 20:25:28 +0200

    [Wizard] Adjust Wizard to Caster interface

--- unit/Wizard.cpp
+++ unit/Wizard.cpp
@@ -1,45 +1,8 @@
 #include "Wizard.h"
 
-Wizard::Wizard(const std::string& name, UnitClass title, int maxHp, int damage, int maxMana) : Soldier(name, title, maxHp, damage) {
-    this->state = new CasterState(name, title, maxHp, damage, maxMana);
-    this->spellbook = new std::map<SpellTitle, Spell*>();
-    
-    this->spellbook->insert(std::pair<SpellTitle, Spell*>(FLAME_STRIKE, new FlameStrike()));
-    this->spellbook->insert(std::pair<SpellTitle, Spell*>(HEAL, new Heal()));
-}
+Wizard::Wizard(const std::string& name, UnitClass title, int maxHp, int damage, int maxMana) : Caster(name, title, maxHp, damage, maxMana) {}
 
-Wizard::~Wizard() {
-    delete state;
-}
-
-const int Wizard::getMana() const {
-    return state->getMana();
-}
-const int Wizard::getMaxMana() const {
-    return state->getMaxMana();
-}
-const std::map<SpellTitle, Spell*>& Wizard::openSpellbook() const {
-    return *spellbook;
-}
-
-void Wizard::reduceMana(int cost) {
-    this->state->reduceMana(cost);
-}
-
-void Wizard::restoreMana(int points) {
-    this->state->restoreMana(points);
-}
-
-Spell& Wizard::findSpell(SpellTitle title) const {
-    Spell* spell;
-    
-    if ( this->openSpellbook().find(title) == this->openSpellbook().end() ) {
-        // throw SpellUnavailableException();
-    }
-    spell = openSpellbook().at(FLAME_STRIKE);
-    
-    return *spell;
-}
+Wizard::~Wizard() {}
 
 void Wizard::cast(SpellTitle title, Unit& target) {
     Spell& spell = findSpell(title);
--- unit/Wizard.h
+++ unit/Wizard.h
@@ -2,30 +2,13 @@
 #define WIZARD_H
 
 #include <iostream>
-#include <map>
-#include "Soldier.h"
-#include "../state/CasterState.h"
-#include "../spell/Spell.h"
-#include "../spell/FlameStrike.h"
-#include "../spell/Heal.h"
+#include "Caster.h"
 
-class Wizard : public Soldier {
-    protected:
-        CasterState* state;
-        std::map<SpellTitle, Spell*>* spellbook;
-    
+class Wizard : public Caster {
     public:
         Wizard(const std::string& name, UnitClass title=WIZARD, int maxHp=50, int damage=3, int maxMana=100);
         virtual ~Wizard();
         
-        const int getMana() const;
-        const int getMaxMana() const;
-        const std::map<SpellTitle, Spell*>& openSpellbook() const;
-        
-        void reduceMana(int cost);
-        void restoreMana(int points);
-        
-        Spell& findSpell(SpellTitle title) const;
         void cast(SpellTitle title, Unit& target);
 };
 
