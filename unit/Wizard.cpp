630826b9b808d61c26df859423009d98b1276206
bor16 <kalininboris@gmail.com>
2016-12-01 16:13:18 +0200

    [Wizard] Give spellbook to Wizard

--- unit/Wizard.cpp
+++ unit/Wizard.cpp
@@ -2,6 +2,10 @@
 
 Wizard::Wizard(const std::string& name, UnitClass title, int maxHp, int damage, int maxMana) : Soldier(name, title, maxHp, damage) {
     this->state = new CasterState(name, title, maxHp, damage, maxMana);
+    this->spellbook = new std::map<SpellTitle, Spell*>();
+    
+    this->spellbook->insert(std::pair<SpellTitle, Spell*>(FLAME_STRIKE, new FlameStrike()));
+    this->spellbook->insert(std::pair<SpellTitle, Spell*>(HEAL, new Heal()));
 }
 
 Wizard::~Wizard() {
@@ -14,6 +18,9 @@ const int Wizard::getMana() const {
 const int Wizard::getMaxMana() const {
     return state->getMaxMana();
 }
+const std::map<SpellTitle, Spell*>& Wizard::openSpellbook() const {
+    return *spellbook;
+}
 
 void Wizard::reduceMana(int cost) {
     this->state->reduceMana(cost);
@@ -23,7 +30,20 @@ void Wizard::restoreMana(int points) {
     this->state->restoreMana(points);
 }
 
-void Wizard::cast(Unit& target, Spell& spell) {
+Spell& Wizard::findSpell(SpellTitle title) const {
+    Spell* spell;
+    
+    if ( this->openSpellbook().find(title) == this->openSpellbook().end() ) {
+        // throw SpellUnavailableException();
+    }
+    spell = openSpellbook().at(FLAME_STRIKE);
+    
+    return *spell;
+}
+
+void Wizard::cast(SpellTitle title, Unit& target) {
+    Spell& spell = findSpell(title);
+    
     this->state->ensureIsAlive();
     
     this->reduceMana(spell.getCost());
--- unit/Wizard.h
+++ unit/Wizard.h
@@ -2,13 +2,17 @@
 #define WIZARD_H
 
 #include <iostream>
+#include <map>
 #include "Soldier.h"
 #include "../state/CasterState.h"
 #include "../spell/Spell.h"
+#include "../spell/FlameStrike.h"
+#include "../spell/Heal.h"
 
 class Wizard : public Soldier {
     protected:
         CasterState* state;
+        std::map<SpellTitle, Spell*>* spellbook;
     
     public:
         Wizard(const std::string& name, UnitClass title=WIZARD, int maxHp=50, int damage=3, int maxMana=100);
@@ -16,11 +20,13 @@ class Wizard : public Soldier {
         
         const int getMana() const;
         const int getMaxMana() const;
+        const std::map<SpellTitle, Spell*>& openSpellbook() const;
         
         void reduceMana(int cost);
         void restoreMana(int points);
         
-        void cast(Unit& target, Spell& spell);
+        Spell& findSpell(SpellTitle title) const;
+        void cast(SpellTitle title, Unit& target);
 };
 
 #endif //WIZARD_H
