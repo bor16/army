f44a41c97b83613e680ebac49234f5b750187ec9
bor16 <kalininboris@gmail.com>
2016-12-04 01:30:41 +0200

    [UnitClass] Move enum UnitClass to separate header

---
+++ state/UnitClass.h
@@ -0,0 +1,17 @@
+#ifndef UNITCLASS_H
+#define UNITCLASS_H
+
+enum class UnitClass {
+    SOLDIER,
+    ROGUE,
+    WEREWOLF,
+    VAMPIRE,
+    BERSERKER,
+    WIZARD,
+    HEALER,
+    PRIEST,
+    WARLOCK,
+    NECROMANCER
+};
+
+#endif //UNITCLASS_H
