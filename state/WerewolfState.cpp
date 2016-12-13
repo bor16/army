9d4aa0c32624b8c91d04b72ec1af9b641e06a4cf
bor16 <kalininboris@gmail.com>
2016-12-13 12:19:17 +0200

    [WerewolfState] Add WerewolfState

---
+++ state/WerewolfState.cpp
@@ -0,0 +1,37 @@
+#include "WerewolfState.h"
+
+WerewolfState::WerewolfState(unitClass title, int maxHp, int damage) : State(title, maxHp, damage) {
+    this->active = new State(title, maxHp, damage);
+    this->alternative = new WolfState(title, maxHp*2, damage*2);
+}
+
+WerewolfState::~WerewolfState() {
+    delete active;
+    delete alternative;
+}
+
+const int WerewolfState::getHp() const {
+    return active->getHp();
+}
+
+void WerewolfState::shapeShift() {
+    State* tmp = active;
+    
+    active = alternative;
+    alternative = tmp;
+}
+
+void WerewolfState::takeDamage(int damage) {
+    this->active->takeDamage(damage);
+    this->alternative->takeDamage(damage);
+}
+
+void WerewolfState::takeMagDamage(int damage) {
+    this->active->takeMagDamage(damage);
+    this->alternative->takeMagDamage(damage);
+}
+
+void WerewolfState::restoreHp(int points) {
+    this->active->restoreHp(points);
+    this->alternative->restoreHp(points);
+}
