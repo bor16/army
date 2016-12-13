4f6f53242d025973ca5c84e22db0e687c1cf6a59
bor16 <kalininboris@gmail.com>
2016-12-13 13:06:23 +0200

    Update tests

--- testing/case.cpp
+++ testing/case.cpp
@@ -1,9 +1,11 @@
 #include "../unit/Soldier.h"
 #include "../unit/Rogue.h"
+#include "../unit/Berserker.h"
 #include "../unit/Wizard.h"
 #include "../unit/Healer.h"
 #include "../unit/Priest.h"
-#include "../unit/Necromancer.h"
+#include "../unit/Werewolf.h"
+// #include "../unit/Necromancer.h"
 #include "../spell/Spell.h"
 #include "catch.hpp"
 
@@ -15,23 +17,23 @@ TEST_CASE("test Soldier", "[Soldier]") {
         REQUIRE( sol->getHp() == 100 );
         REQUIRE( sol->getMaxHp() == 100 );
         REQUIRE( sol->getDamage() == 10 );
-        REQUIRE( sol->getTitle() == Class::SOLDIER );
+        REQUIRE( sol->getTitle() == unitClass::SOLDIER );
     }
     
     SECTION("Attack") {
-        sol->attack(*sol2);
+        sol->attack(*sol2, *sol);
         REQUIRE( sol2->getHp() == ((int)Hp::SOLDIER - (int)Dmg::SOLDIER) );
         REQUIRE( sol->getHp() == ((int)Hp::SOLDIER - (int)Dmg::SOLDIER/2) );
     }
     
     SECTION("Limit") {
         for ( int i = 0; i < 16; i++ ) {
-            sol->attack(*sol2);
+            sol->attack(*sol2, *sol);
         }
         REQUIRE( sol2->getHp() >= 0 );
         REQUIRE( sol->getHp() == 55 );
         
-        REQUIRE_THROWS_AS( sol2->attack(*sol), DeadActionException );
+        REQUIRE_THROWS_AS( sol2->attack(*sol, *sol2), DeadActionException );
     }
     
     delete sol;
@@ -47,11 +49,11 @@ TEST_CASE("test Rogue", "[Rogue]") {
         REQUIRE( rog->getHp() == 60 );
         REQUIRE( rog->getMaxHp() == 60 );
         REQUIRE( rog->getDamage() == 8 );
-        REQUIRE( rog->getTitle() == Class::ROGUE );
+        REQUIRE( rog->getTitle() == unitClass::ROGUE );
     }
     
     SECTION("Attack") {
-        sol->attack(*rog);
+        sol->attack(*rog, *sol);
         REQUIRE( rog->getHp() == ((int)Hp::ROGUE - (int)Dmg::SOLDIER) );
         REQUIRE( sol->getHp() == ((int)Hp::SOLDIER - (int)Dmg::ROGUE/2) );
         
@@ -60,24 +62,35 @@ TEST_CASE("test Rogue", "[Rogue]") {
         REQUIRE( rog->getHp() == (int)Hp::ROGUE );
         REQUIRE( sol->getHp() == (int)Hp::SOLDIER );
         
-        rog->attack(*sol);
+        rog->attack(*sol, *rog);
         REQUIRE( sol->getHp() == ((int)Hp::SOLDIER - (int)Dmg::ROGUE) );
         REQUIRE( rog->getHp() == (int)Hp::ROGUE );
     }
     
     SECTION("Limit") {
         for ( int i = 0; i < 16; i++ ) {
-            rog->attack(*rog2);
+            rog->attack(*rog2, *rog);
         }
         REQUIRE( rog2->getHp() >= 0 );
         REQUIRE( rog->getHp() == (int)Hp::ROGUE );
         
-        REQUIRE_THROWS_AS( rog2->attack(*rog), DeadActionException );
+        REQUIRE_THROWS_AS( rog2->attack(*rog, *rog2), DeadActionException );
     }
     
     delete sol;
     delete rog;
     delete rog2;
+}
+
+TEST_CASE("test Berserker", "[Berserker]") {
+    Berserker* ber = new Berserker();
+    
+    SECTION("TakeMagDamage") {
+        ber->takeMagDamage(16);
+        REQUIRE( ber->getHp() == (int)Hp::BERSERKER );
+    }
+    
+    delete ber;
 }
 
 TEST_CASE("test Wizard", "[Wizard]") {
@@ -91,7 +104,7 @@ TEST_CASE("test Wizard", "[Wizard]") {
         REQUIRE( wiz->getMaxHp() == 60 );
         REQUIRE( wiz->getDamage() == 4 );
         REQUIRE( wiz->getMana() == 200 );
-        REQUIRE( wiz->getTitle() == Class::WIZARD );
+        REQUIRE( wiz->getTitle() == unitClass::WIZARD );
     }
     
     SECTION("Cast") {
@@ -118,5 +131,47 @@ TEST_CASE("test Wizard", "[Wizard]") {
     delete wiz;
     delete hea;
     delete pri;
+    delete sol;
+}
+
+TEST_CASE("test Werewolf", "[Werewolf]") {
+    Werewolf* wer = new Werewolf();
+    Soldier* sol = new Soldier();
+    Wizard* wiz = new Wizard();
+    
+    SECTION("shapeShift") {
+        CHECK( wer->getHp() == (int)Hp::WEREWOLF );
+        
+        wer->shapeShift();
+        CHECK( wer->getHp() == (int)Hp::WEREWOLF*2 );
+        
+        wer->shapeShift();
+        CHECK( wer->getHp() == (int)Hp::WEREWOLF );
+    }
+    
+    SECTION("shapeShift + damage") {
+        REQUIRE( wer->getHp() == (int)Hp::WEREWOLF );
+        
+        sol->attack(*wer, *sol);
+        CHECK( wer->getHp() == ((int)Hp::WEREWOLF - (int)Dmg::SOLDIER) );
+        wer->shapeShift();
+        CHECK( wer->getHp() == ((int)Hp::WEREWOLF*2 - (int)Dmg::SOLDIER) );
+        wer->shapeShift();
+        CHECK( wer->getHp() == ((int)Hp::WEREWOLF - (int)Dmg::SOLDIER) );
+        
+        wer->restoreHp(1000);
+        REQUIRE( wer->getHp() == (int)Hp::WEREWOLF );
+        
+        sol->attack(*wer, *sol);
+        CHECK( wer->getHp() == ((int)Hp::WEREWOLF - (int)Dmg::SOLDIER) );
+        wer->shapeShift();
+        sol->attack(*wer, *sol);
+        CHECK( wer->getHp() == ((int)Hp::WEREWOLF*2 - (int)Dmg::SOLDIER*2) );
+        wer->shapeShift();
+        CHECK( wer->getHp() == ((int)Hp::WEREWOLF - (int)Dmg::SOLDIER*2) );
+    }
+    
+    delete wer;
+    delete wiz;
     delete sol;
 }
