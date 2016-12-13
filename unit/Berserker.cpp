8ac9710eddbb1d970cca797ab3d44a0eaba6efcc
bor16 <kalininboris@gmail.com>
2016-12-13 12:30:31 +0200

    [Berserker] Fix Berserker (immune to magic attacks)

--- unit/Berserker.cpp
+++ unit/Berserker.cpp
@@ -1,9 +1,13 @@
 #include "Berserker.h"
 
-Berserker::Berserker(Class title, int maxHp, int damage) : Soldier(title, maxHp, damage) {
+Berserker::Berserker(unitClass title, int maxHp, int damage) : Soldier(title, maxHp, damage) {
     this->state = new BerserkerState(title, maxHp, damage);
 }
 
 Berserker::~Berserker() {
     delete state;
+}
+
+void Berserker::takeMagDamage(int damage) {
+    this->state->takeMagDamage(damage);
 }
